447de5ef5959ebc466866bd8fee9fec4806839b8
Kund0249 <k.kundan1999@gmail.com>
2023-07-11 20:35:46 +0530

    Methods

---
+++ NvestApp/OOPS/Methods/Demo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace NvestApp.OOPS.Methods
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+            //create an instance of class
+            MathUtil math = new MathUtil();
+
+            //bool Isprime = math.IsPrime(n);
+
+            //if (Isprime)
+            //    Console.WriteLine("{0} : Prime", n);
+            //else
+            //    Console.WriteLine("{0} : Not Prime", n);
+
+
+            //if (math.IsPrime(n))
+            //    Console.WriteLine("{0} : Prime", n);
+            //else
+            //    Console.WriteLine("{0} : Not Prime", n);
+
+            //int sum = math.Add(10, 20);
+            //Console.WriteLine(sum);
+            //math.EvenNumber(20, 10);
+
+            int[] EvenNumbers = math.GetAllEven(1, 15);
+
+            //foreach (int item in EvenNumbers)
+            //{
+            //    Console.Write(item + "\t");
+            //}
+            //Console.WriteLine();
+
+            ArrayList list = math.GetEvenNumbers(1, 15);
+
+            foreach (int item in list)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ NvestApp/OOPS/Methods/MathUtil.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+
+namespace NvestApp.OOPS.Methods
+{
+    class MathUtil
+    {
+        //access-specifiers returnType Name([arg])
+        //{//body}
+
+        public bool IsPrime(int value)
+        {
+            //bool IsPrime = true;
+            for (int i = 2; i < value; i++)
+            {
+                if (value % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            //if (IsPrime)
+            //    Console.WriteLine("{0} is prime number", value);
+            //else
+            //    Console.WriteLine("{0} is not prime number", value);
+            //return IsPrime;
+            return true;
+        }
+
+        public int Add(int V1, int V2)
+        {
+            //int sum = V1 + V2;
+            //return sum;
+            return (V1 + V2);
+        }
+
+        
+        public void EvenNumber(int V1, int V2)
+        {
+            for (int i = (V1 < V2 ? V1 : V2); i <= (V2 > V1 ? V2 : V1); i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        public int[] GetAllEven(int V1,int V2)
+        {
+            //Step 1 : Check how many even number is there
+            int Counter = 0;
+            for (int i = (V1 < V2 ? V1 : V2); i <= (V2 > V1 ? V2 : V1); i++)
+            {
+                if (i % 2 == 0)
+                    Counter++;
+            }
+
+            //Step 2 : create array to store all even number
+            int[] EvenNumbers = new int[Counter];
+
+
+            //Step 3 : insert even number in array
+            int Index = 0;
+            for (int i = (V1 < V2 ? V1 : V2); i <= (V2 > V1 ? V2 : V1); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    EvenNumbers[Index] = i;
+                    Index++;
+                }
+                    
+            }
+
+            return EvenNumbers;
+        }
+
+        public ArrayList GetEvenNumbers(int V1, int V2)
+        {
+            ArrayList list = new ArrayList();
+            for (int i = (V1 < V2 ? V1 : V2); i <= (V2 > V1 ? V2 : V1); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+    }
+}
